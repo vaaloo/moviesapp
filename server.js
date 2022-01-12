bc5f5aadc94fee33c6f8d5c3018d7e7cd7bcacd1
Valentin <lesrageursdu13@gmail.com>
2022-01-12 22:16:00 +0100

    🎥Film page added

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.get('/', (req, res) => {
     res.render("index")
 })
 
+app.get("/series", (req, res) => {
+    res.render("series")
+})
+
+app.get("/films", (req, res) => {
+    res.render("films")
+})
+
 app.get("/getTv", async (req, res) => {
     let tvName = req.query.tvName;
     try {
@@ -47,7 +55,45 @@ app.get("/getTv", async (req, res) => {
                 })
             }
         }
-        res.render("filmpage", {
+        res.render("seriePage", {
+            medias: arrayMedia
+        })
+      } catch (error) {
+        console.error(error);
+      }
+})
+
+app.get("/getMovie", async (req, res) => {
+    let movieName = req.query.movieName;
+    try {
+        const args = {
+            query: {
+              query: movieName,
+            },
+        };
+        const movie = await mdb.search.movies(args)
+        let results = movie.data.results
+        let arrayMedia = new Array()
+        if (results.length >= 5) {
+            for (let i = 0; i < 5; i++) {
+                let result = results[i]
+                arrayMedia.push({
+                    name: result.title,
+                    image: result.poster_path,
+                    desc: result.overview
+                })
+            }
+        } else {
+            for (let i = 0; i < results.length; i++) {
+                let result = results[i]
+                arrayMedia.push({
+                    name: result.title,
+                    image: result.poster_path,
+                    desc: result.overview
+                })
+            }
+        }
+        res.render("filmsPage", {
             medias: arrayMedia
         })
       } catch (error) {
