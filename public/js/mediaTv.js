8030f854cb090b5dab27af5be16ac12d2bb1ac71
Valentin <lesrageursdu13@gmail.com>
2022-01-13 14:11:43 +0100

    🖥️Films and series details added

---
+++ public/js/mediaTv.js
@@ -0,0 +1,10 @@
+let medias = document.querySelectorAll(".media-container")
+for (let i = 0; i < medias.length; i++) {
+    let media = medias[i];
+    if (media) {
+        media.addEventListener("click", (e) => {
+            let id = media.getAttribute("id")
+            window.location = `/tv/${id}`
+        })
+    }
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { TextChannel, CommandInteractionOptionResolver } = require("discord.js");
 const express = require("express");
 const app = express();
 let apiKey = process.env.THEMOVIEDB_API_KEY
@@ -42,21 +43,24 @@ app.get("/getTv", async (req, res) => {
                 arrayMedia.push({
                     name: result.name,
                     image: result.poster_path,
-                    desc: result.overview
+                    desc: result.overview,
+                    id: result.id
                 })
             }
         } else {
             for (let i = 0; i < results.length; i++) {
                 let result = results[i]
                 arrayMedia.push({
-                    name: result.original_name,
+                    name: result.name,
                     image: result.poster_path,
-                    desc: result.overview
+                    desc: result.overview,
+                    id: result.id
                 })
             }
         }
         res.render("seriePage", {
-            medias: arrayMedia
+            medias: arrayMedia,
+            search: tvName,
         })
       } catch (error) {
         console.error(error);
@@ -80,7 +84,8 @@ app.get("/getMovie", async (req, res) => {
                 arrayMedia.push({
                     name: result.title,
                     image: result.poster_path,
-                    desc: result.overview
+                    desc: result.overview,
+                    id: result.id
                 })
             }
         } else {
@@ -89,17 +94,66 @@ app.get("/getMovie", async (req, res) => {
                 arrayMedia.push({
                     name: result.title,
                     image: result.poster_path,
-                    desc: result.overview
+                    desc: result.overview,
+                    id: result.id
                 })
             }
         }
         res.render("filmsPage", {
-            medias: arrayMedia
+            medias: arrayMedia,
+            search: movieName
         })
       } catch (error) {
         console.error(error);
       }
 })
+
+app.get("/tv/:id", async (req, res) => {
+    let id = req.params.id;
+    try {
+        const args = {
+            pathParameters: {
+                tv_id: id
+            },
+        };
+        const tv = await mdb.tv.getDetails(args);
+        res.render("serieAdv", {
+            name: tv.data.name,
+            year: `${tv.data.first_air_date}`,
+            img: tv.data.poster_path,
+            desc: tv.data.overview,
+            seasons: tv.data.number_of_seasons,
+            episodes: tv.data.number_of_episodes,
+            note: String(tv.data.vote_average),
+            creators: tv.data.created_by
+        })
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.get("/movie/:id", async (req, res) => {
+    let id = req.params.id;
+    try {
+        const args = {
+            pathParameters: {
+                movie_id: id
+            },
+        };
+        const movie = await mdb.movie.getDetails(args);
+        res.render("filmAdv", {
+            name: movie.data.title,
+            year: `${movie.data.release_date}`,
+            img: movie.data.poster_path,
+            desc: movie.data.overview,
+            note: String(movie.data.vote_average),
+            budget: String(movie.data.budget)
+        })
+    } catch (error) {
+        console.error(error)
+    }
+})
+
 
 app.listen("8080", () => {
     console.log("App démarrée sur le port 8080")
